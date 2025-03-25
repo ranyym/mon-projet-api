fc47f2f5b2557902f36331dd18b7e069b0729c0c
ranyym <ranimferjeoui16@gmail.com>
2025-03-25 15:45:50 +0100

    Correction du port

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require("express");
 const cors = require("cors");
 
 const app = express();
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || 1000;
 
 app.use(express.json()); // Accepter les données JSON
 app.use(cors()); // Autoriser les requêtes depuis le front-end
